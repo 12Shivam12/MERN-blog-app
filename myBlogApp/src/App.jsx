17c141ec1ad308843122c02938da7ab4f54d2e52
12Shivam12 <120035361+12Shivam12@users.noreply.github.com>
2024-05-12 22:55:44 +0530

    added functionality in signup page

--- myBlogApp/src/App.jsx
+++ myBlogApp/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Signin from "./pages/Signin"
-import Signup from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Header from "./components/Header"
+import SignIn from "./pages/Signin"
+import SignUp from "./pages/Signup"
 
 
 function App() {
@@ -17,8 +17,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<Signin />} />
-        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
