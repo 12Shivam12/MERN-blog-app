28a5c0fdeb4f49a9cab1737fbace8b65f9bf0c6f
12Shivam12 <120035361+12Shivam12@users.noreply.github.com>
2024-05-13 19:54:58 +0530

    added redux toolkit

--- myBlogApp/src/pages/Signin.jsx
+++ myBlogApp/src/pages/Signin.jsx
@@ -1,12 +1,16 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Signin = () => {
 
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+
+  const { loading, error: errorMessage } = useSelector(state => state.user)
+
+  const dispatch = useDispatch();
 
   const navigate = useNavigate();
 
@@ -18,12 +22,11 @@ const Signin = () => {
     e.preventDefault();
 
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields');
+      return dispatch(signInFailure('Please fill all the fields'))
     }
 
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -31,15 +34,14 @@ const Signin = () => {
       });
       const data = await res.json()
       if (data.success === false) {
-        return setErrorMessage(data.message)
+        dispatch(signInFailure(data.message))
       }
-      setLoading(false)
-      if(res.ok){
+      if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/')
       }
     } catch (error) {
-      setErrorMessage(error.message)
-      setLoading(false);
+      dispatch(signInFailure(error.message))
     }
   }
 
@@ -60,7 +62,7 @@ const Signin = () => {
         {/* right side */}
         <div className="flex-1">
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-           
+
             <div>
               <Label value='Your email' />
               <TextInput onChange={handleChange}
@@ -83,7 +85,7 @@ const Signin = () => {
                   </>
                 ) : 'Sign In'
               }
-              
+
             </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
