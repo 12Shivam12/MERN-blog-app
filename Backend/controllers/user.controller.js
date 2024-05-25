cd38de2a96b26cac112323121ffbc9c0ac132c71
12Shivam12 <120035361+12Shivam12@users.noreply.github.com>
2024-05-25 22:58:30 +0530

    completed delete user functionality

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -56,6 +56,6 @@ export const deleteUser = async(req,res,next) =>{
         await User.findByIdAndDelete(req.params.userId);
         res.status(200).json('User has been deleted');
     } catch (error) {
-        
+        next(error)
     }
 }
