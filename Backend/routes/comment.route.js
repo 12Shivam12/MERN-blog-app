a2a874ad8363988a9fd5ed57adefd24598b844c5
12Shivam12 <120035361+12Shivam12@users.noreply.github.com>
2024-05-27 22:51:26 +0530

    show the comments of a post

--- Backend/routes/comment.route.js
+++ Backend/routes/comment.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createComment } from '../controllers/Comment.controller.js';
+import { createComment, getPostComments } from '../controllers/Comment.controller.js';
 import {verifyToken} from '../utils/verifyUser.js'
 
 const router = express.Router();
 
 router.post('/create', verifyToken , createComment);
+router.get('/getPostComments/:postId', getPostComments);
 
 export default router;
