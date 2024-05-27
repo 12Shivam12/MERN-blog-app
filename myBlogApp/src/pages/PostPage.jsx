22b248407ae97fa41e590c2226ef1e214c3c39dd
12Shivam12 <120035361+12Shivam12@users.noreply.github.com>
2024-05-27 17:31:33 +0530

    added callToActin in post page

--- myBlogApp/src/pages/PostPage.jsx
+++ myBlogApp/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import CallToActions from '../components/CallToActions';
 
 const PostPage = () => {
     const {postSlug} = useParams();
@@ -52,6 +53,9 @@ const PostPage = () => {
         </div>
         <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
 
+        </div>
+        <div className="max-w-4xl mx-auto w-full">
+            <CallToActions/>
         </div>
     </main>
   )
